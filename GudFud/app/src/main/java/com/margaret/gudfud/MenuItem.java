1b155670731f66f65563ea456931e01080e042ee
margaretmcrawf <margaretmcrawf@gmail.com>
2016-10-06 20:00:31 -0400

    added methods to arrayadapter

---
+++ GudFud/app/src/main/java/com/margaret/gudfud/MenuItem.java
@@ -0,0 +1,7 @@
+package com.margaret.gudfud;
+
+/**
+ * MenuItem class which consists of a name, and some other stuff like ingredients.
+ */
+public class MenuItem {
+}
--- GudFud/app/src/main/java/com/margaret/gudfud/MenuItemsListAdapter.java
+++ GudFud/app/src/main/java/com/margaret/gudfud/MenuItemsListAdapter.java
@@ -1,7 +1,40 @@
 package com.margaret.gudfud;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * A list adapter for the menu items. .
  */
-public class MenuItemsListAdapter {
-}
+public class MenuItemsListAdapter extends ArrayAdapter<MenuItem>{
+    @BindView(R.id.tvText) TextView tvtext;
+    private ArrayList<MenuItem> items;
+
+    public MenuItemsListAdapter(Context context, ArrayList<MenuItem> items) {
+        super(context, 0, items);
+        this.items = items;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final MenuItem item = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.menu_item, parent, false);
+        }
+
+
+
+        // Return the completed view to render on screen
+        return convertView;
+        }
+    }
