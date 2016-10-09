69871d0172bf81ed9a607c9a59b2d9c2d6b36401
lnielsen <louiseenielsen@gmail.com>
2016-10-09 15:56:33 -0400

    can see login page on phone

--- GudFud/app/src/main/java/com/margaret/gudfud/MainActivity.java
+++ GudFud/app/src/main/java/com/margaret/gudfud/MainActivity.java
@@ -1,13 +1,26 @@
 package com.margaret.gudfud;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final LoginFragment loginFragment = new LoginFragment();
+        if (savedInstanceState== null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.holder, loginFragment).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
